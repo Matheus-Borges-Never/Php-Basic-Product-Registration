61bb0df5152257ecc9f0d7d90c67739b81f4901d
Matheus-Borges <84951351+Matheus-Borges-Never@users.noreply.github.com>
2022-06-09 17:07:17 -0300

    Second commit

--- atv_06/mostrarDados.php
+++ atv_06/mostrarDados.php
@@ -17,16 +17,32 @@
         $sql = "select *, vlunit *qtde as total from produtos";
         $total = 0;
         $ret = $con -> query($sql);
+        $m = "<table border='1' width='600' cellsapacing='0'>";
+        $m .= "<tr bgcolor='yellow'>
+                    <th>Codigo</th><th>Produto</th><th>Quantidade</th><th>Valor</th><th>Total</th>
+                </tr>";
 
         while ($registro = $ret -> fetch_assoc()){
             //Exibe os registros retornados pelo comando SQL executado
-            echo "Produto: " . $registro['produto'];
-            echo " , Quantidade: " . $registro['qtde'];
-            echo " , Valor Unitario: " . $registro ['vlunit'];
-            echo " , Total: " . $registro ['total'];
-            echo "<br> <br>";
+            $m .= "
+                    <tr >
+                        <td>" . $registro['codigo'] . " </td>
+                        <td>" . $registro['produto'] . "    </td>
+                        <td>" . $registro['qtde'] . "   </td>
+                        <td>" . $registro['vlunit'] . " </td>
+                        <td>" . $registro['total'] . "  </td>
+                    </tr>
+                    ";
+
+            // echo "Produto: " . $registro['produto'];
+            // echo " , Quantidade: " . $registro['qtde'];
+            // echo " , Valor Unitario: " . $registro ['vlunit'];
+            // echo " , Total: " . $registro ['total'];
+            // echo "<br> <br>";
             $total += $registro['total'];
         }
+        $m .= "</table>";
+        echo $m;
         echo "<h6>Total: $total</h6>";
 
 
